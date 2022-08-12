21746df99a190301aa426e524021ab1533d415a3
Amit-Khobragade <amitkh1102@outlook.com>
2022-08-12 23:37:56 +0530

    Added the first component

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import App from "./src/App";
 
-createRoot(document.getElementById("root")).render(<h1>Hello</h1>);
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App></App>
+  </React.StrictMode>
+);
