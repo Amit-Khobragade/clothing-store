105ae451d73d36e8fab11390c1185e6cae7b44c5
Amit-Khobragade <amitkh1102@outlook.com>
2022-08-15 13:04:16 +0530

    Navbar is split in components to better modularize the navbar
    Navbar is split in 3 component, this includes:
    	- Logo
    	- SearchBox
    	- UserControls
    The Logo is an svg element while the searchbox is a div containing the
    search box and the icon and user control is a div containing the user controls
    all the styles for these elements will be provided later.

--- src/components/NavBar/NavBar.component.jsx
+++ src/components/NavBar/NavBar.component.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Logo from "assets/Generic_store.svg"
+import SearchBox from "components/SearchBox/SearchBox.component";
+import UserControls from "components/UserControls/UserControls.component";
 
 
-const NavBar = () => {
-  return (<div>
-    <h1>NavBar</h1>
-    <Outlet></Outlet>
-  </div>)
-}
+const NavBar = () => (
+  <div>
+    <Logo />
+    <SearchBox />
+    <UserControls />
+  </div>
+)
 
 export default NavBar;
