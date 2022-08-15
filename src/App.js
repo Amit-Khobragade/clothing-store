a1884fb8f34f55e3ff9761d9fbb2c00e597d2fe6
Amit-Khobragade <amitkh1102@outlook.com>
2022-08-15 13:22:37 +0530

    Exchanged the Navbar with Body tag
    This was done to maintain a structure to the app of it containing a
    navbar and footer and the content appearing between the navbar and the
    footer

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import { Route, Routes } from "react-router-dom";
 import NavBar from "components/NavBar/NavBar.component";
 import Home from "./Home";
 import AuthPage from "auth/AuthPage";
+import Body from "components/Body/Body.component";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<NavBar></NavBar>}>
+      <Route path="/" element={<Body />}>
         <Route path="/" element={<Home />} exact />
         <Route path="/auth" element={<AuthPage />} exact />
       </Route>
