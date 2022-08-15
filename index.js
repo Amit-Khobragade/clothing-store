3bdf1b4072dcefe3d7872507cc08010c14e05438
Amit-Khobragade <amitkh1102@outlook.com>
2022-08-15 12:52:27 +0530

    Removed unrequired dependencies

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./src/App";
-import { BrowserRouter, MemoryRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
