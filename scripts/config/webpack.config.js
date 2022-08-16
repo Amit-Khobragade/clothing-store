830425acd6efa638403ba6031eccd49ab90ab2d7
Amit-Khobragade <amitkh1102@outlook.com>
2022-08-16 06:48:37 +0530

    Moved and changed webpack exports
    Webpack.config consisted of a dev config for webpack. for future
    production purposes it was switched to a function which can be
    used to gain the webpack configuration based on enviornment
    variable 'isDev' which is set by configure script

---
+++ scripts/config/webpack.config.js
@@ -0,0 +1,90 @@
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const paths = require("./paths");
+
+const { isDev } = process.env;
+
+if (!isDev) {
+  throw new Error("config not found");
+}
+
+module.exports = () => ({
+  devServer: isDev
+    ? {
+        static: { directory: paths.publicDir },
+        compress: true,
+        port: 3000,
+        hot: true,
+        historyApiFallback: true,
+      }
+    : null,
+  entry: paths.IndexJSPath,
+  mode: isDev ? "development" : "production",
+  output: {
+    path: paths.buildDir,
+    filename: isDev
+      ? "static/js/[name].[contenthash:8].chunk.js"
+      : "static/js/[name].chunk.js",
+    assetModuleFilename: isDev
+      ? "static/media/[name][ext]"
+      : "static/media/[name].[hash][ext]",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.?jsx?$/,
+        exclude: /(node_modules|config)/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-react"],
+            },
+          },
+        ],
+      },
+      {
+        test: /.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /.(png|jpe?g)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /.svg$/,
+        use: ["@svgr/webpack"],
+      },
+      {
+        test: /.s[ac]ss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: paths.IndexHTMLPath,
+      minify: isDev
+        ? false
+        : {
+            collapseWhitespace: true,
+            keepClosingSlash: true,
+            minifyCSS: true,
+            minifyJS: true,
+            minifyURLs: true,
+            removeComments: true,
+            removeEmptyAttributes: true,
+            removeRedundantAttributes: true,
+            useShortDoctype: true,
+          },
+    }),
+  ],
+  resolve: {
+    extensions: [".js", ".json", ".wasm", ".jsx", ".component.jsx"],
+    alias: {
+      components: paths.componentsDir,
+      assets: paths.assetsDir,
+      auth: paths.authDir,
+    },
+  },
+  devtool: "eval-source-map",
+});
