bd639191c3b775c9f7af63e92b73850101c73a98
Amit-Khobragade <amitkh1102@outlook.com>
2022-08-16 19:57:13 +0530

    Removed uneeded deps

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import NavBar from "components/NavBar/NavBar.component";
 import Home from "./Home";
 import AuthPage from "auth/AuthPage";
 import Body from "components/Body/Body.component";
--- src/components/NavBar/NavBar.component.jsx
+++ src/components/NavBar/NavBar.component.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
 import Logo from "assets/Generic_store.svg"
 import SearchBox from "components/SearchBox/SearchBox.component";
 import UserControls from "components/UserControls/UserControls.component";
