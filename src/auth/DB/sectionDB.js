186825b3d0024c5bf398d0141932f950d13d3727
Amit-Khobragade <amitkh1102@outlook.com>
2022-08-17 15:35:50 +0530

    Added a file to acess the sections collection
    This collection is a read only firestore collection. It contains the
    information about sections and their subsections this when arranged
    provides us with a object for rendering the home page easily.

---
+++ src/auth/DB/sectionDB.js
@@ -0,0 +1,16 @@
+import { collection, doc, getDocs, query } from "firebase/firestore";
+import { store } from "../app/firebase";
+
+export const getSections = async () => {
+  const q = query(collection(store, "sections"));
+  const snapshot = await getDocs(q);
+  const sections = {};
+
+  // populating the sections
+  snapshot.docs.forEach((section) => {
+    const data = section.data();
+    sections[data.faction] = data.contents;
+  });
+
+  return sections;
+};
