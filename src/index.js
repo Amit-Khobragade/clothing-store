1c1ab7a24fbd6a779386f978c41660a5453c94fd
Amit-Khobragade <amitkh1102@outlook.com>
2022-08-18 18:18:33 +0530

    Added contexts to index
    The contexts Sections and Offers were added to index this was done to
    ensure that the api call to firebase remain minimal and all the
    components which use section or offer can recive the data at the same
    time.
    It will further reduce the load time of the page after the first load.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { SectionContextProvider } from "context/SectionContext/SectionContext";
+import { OffersContextProvider } from "context/OffersContext/OffersContext";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App></App>
+      <SectionContextProvider>
+        <OffersContextProvider>
+          <App></App>
+        </OffersContextProvider>
+      </SectionContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
