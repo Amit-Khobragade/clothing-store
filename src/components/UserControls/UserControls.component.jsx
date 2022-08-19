4d663917a042bcc83e6b190b38fa90d831a57386
Amit-Khobragade <amitkh1102@outlook.com>
2022-08-20 01:25:44 +0530

    Added styles to UserControls
    These controls focus on providing the user with controls of their
    account. a dropdown menu was designed for this and styled with the
    component to produce a seemless effect. All the transitions were added
    in styled components.

--- src/components/UserControls/UserControls.component.jsx
+++ src/components/UserControls/UserControls.component.jsx
@@ -1,15 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import WishListIcon from 'assets/heart.svg'
 import CartIcon from 'assets/cart.svg'
 import AccountIcon from 'assets/account.svg'
-import StyledUserControls from "./UserControls.styles";
+import { DropDownUserController, StyledUserControls, UserController } from "./UserControls.styles";
+import { Link } from "react-router-dom";
 
-const UserControls = (props) => (
-  <StyledUserControls>
-    <WishListIcon />
-    <CartIcon />
-    <AccountIcon />
-  </StyledUserControls>
-)
+const UserControls = (props) => {
 
+  const [[wishlistState, cartState, accountState], setCurrent] = useState([0, 0, 0]);
+  const toggleWishlist = () => toggleComponent(setCurrent, 0, wishlistState);
+  const toggleCart = () => toggleComponent(setCurrent, 1, cartState);
+  const toggleAccount = () => toggleComponent(setCurrent, 2, accountState);
+  const closeAll = () => setCurrent([0, 0, 0]);
+
+  return (
+    <StyledUserControls current={[wishlistState, cartState, accountState]} onMouseLeave={closeAll}>
+
+      {/* The Button componets with their svgs are placed in this area  */}
+      <UserController current={wishlistState} onClick={toggleWishlist}>
+        <WishListIcon />
+        <h3>wishlist</h3>
+      </UserController>
+      <UserController current={cartState} onClick={toggleCart}>
+        <CartIcon />
+        <h3>cart</h3>
+      </UserController>
+      <UserController current={accountState} onClick={toggleAccount}>
+        < AccountIcon />
+        <h3>account</h3>
+      </UserController>
+
+      {/* The DropDown Menu for each is provided below */}
+      <DropDownUserController current={wishlistState}>
+        <div></div>
+        <Link to={"/auth"}>
+          Sign in to continue
+        </Link>
+      </DropDownUserController>
+      <DropDownUserController current={cartState}>
+        <div></div>
+        <Link to={"/auth"}>
+          Sign in to continue
+        </Link>
+      </DropDownUserController>
+      <DropDownUserController current={accountState}>
+        <div></div>
+        <Link to={"/auth"}>
+          Sign in to continue
+        </Link>
+      </DropDownUserController>
+    </StyledUserControls >
+  )
+
+}
+
+function toggleComponent(setState, position, current) {
+  if (current === 1) {
+    setState([0, 0, 0]);
+    return;
+  }
+
+  const newState = [-1, -1, -1];
+  newState[position] = 1;
+  setState(newState);
+}
 export default UserControls;
--- src/components/UserControls/UserControls.styles.js
+++ src/components/UserControls/UserControls.styles.js
@@ -1,9 +1,82 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-const StyledUserControls = styled.div`
+export const StyledUserControls = styled.div`
   align-items: center;
   display: flex;
   gap: 20px;
+  justify-content: flex-start;
+  width: 232px;
+
+  ${(props) =>
+    props.current.includes(1) &&
+    `
+      flex-direction: column;
+      background: rgba(255, 255, 255, 0.5);
+      border-radius: 10px 10px 0 0;
+      position: relative;
+  `}
 `;
 
-export default StyledUserControls;
+export const UserController = styled.div`
+  align-items: center;
+  background: transparent;
+  flex-grow: 0.00001;
+  justify-content: space-evenly;
+  transition: all 500ms linear;
+  width: 64px;
+  ${(props) => {
+    if (props.current === 1) {
+      return css`
+        display: flex;
+        flex-grow: 1;
+        width: 100%;
+      `;
+    } else if (props.current === 0) {
+      return css`
+        display: inline-block;
+      `;
+    } else {
+      return css`
+        display: none;
+      `;
+    }
+  }};
+
+  & > svg {
+    display: ${(props) => (props.current === -1 ? "none" : "inline-block")};
+  }
+
+  & > h3 {
+    display: ${(props) => (props.current === 1 ? "inline-block" : "none")};
+  }
+`;
+
+export const DropDownUserController = styled.div`
+  bottom: 0;
+  background: white;
+  border-radius: 0 0 10px 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  overflow: hidden;
+  position: absolute;
+  width: 100%;
+  height: 0;
+  transition: all 250ms linear, transform 500ms;
+
+  ${(props) =>
+    props.current === 1 &&
+    css`
+      height: 300px;
+      transform: translateY(100%);
+      transition: all 500ms linear, transform 250ms;
+    `}
+
+  & div {
+    flex-basis: 80%;
+  }
+  & a {
+    color: var(--text-dark-clr);
+    text-align: center;
+  }
+`;
