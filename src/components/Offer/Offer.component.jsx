7985cbe413e4c25f9333ba11a066d1c86046649b
Amit-Khobragade <amitkh1102@outlook.com>
2022-08-21 18:09:10 +0530

    Added support for the isGrouped prop
    The isGrouped prop determines the style of the Offer Component and
    determines whether to provide a filter or not. This filter is added to
    see dark colored text present on the bottom of the banner.

--- src/components/Offer/Offer.component.jsx
+++ src/components/Offer/Offer.component.jsx
@@ -2,17 +2,17 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { StyledFilterComponent, StyledOffersComponent } from "./Offer.styles";
 
-const Offer = ({ redirectURL, imageURL, content }) => {
+const Offer = ({ redirectURL, imageURL, content, isGrouped }) => {
   const [isImageLoaded, setIsImageLoaded] = useState(false);
 
   return (
     <Link to={redirectURL}>
-      <div className="offer">
+      <StyledOffersComponent isGrouped={isGrouped}>
         <img src={imageURL} onLoad={() => setIsImageLoaded(true)} />
         {!isImageLoaded && <h1>loading offer</h1>}
-        <div className="filter"></div>
+        {!isGrouped && <StyledFilterComponent></StyledFilterComponent>}
         <h1>{content}</h1>
-      </div>
+      </StyledOffersComponent>
     </Link>
   )
 }
