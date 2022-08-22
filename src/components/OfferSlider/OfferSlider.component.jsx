7c3a53af2de07ff94c8fc684eca2f7e71cda1d52
Amit-Khobragade <amitkh1102@outlook.com>
2022-08-22 20:12:11 +0530

    Added variables to handle the scroll
    The various variables and functions handle the scroll component. The
    implementation is done by using ref and events.
    
    The isInactive state has 2 variables left and right which informs the
    component about which button to deactivate depending on the scroll
    position.
    
    The assignment of this state is handled by scrollController which
    focuses on handling the scroll event on the given divs.
    
    The left and right functions are used to change the current scroll
    position of the element. The intensity of this scroll can be adjusted by
    ScrollLength variable set at the start of the function.

--- src/components/OfferSlider/OfferSlider.component.jsx
+++ src/components/OfferSlider/OfferSlider.component.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { createRef, useEffect, useState } from "react";
 import Offer from "components/Offer/Offer.component";
-import StyledOfferSlider from "./OfferSlider.styles";
+import { SliderButton, StyledSliderComponent } from "./OfferSlider.styles";
 
 const OfferSlider = ({ offerGroup }) => {
+  const scrollLength = 1000;
+  const slider = createRef();
+  const [isInactive, setIsInactive] = useState({ left: true });
+
+  const scrollController = ({ target }) => {
+    if (target.scrollLeft === 0) setIsInactive({ left: true });
+    else if (target.scrollWidth - target.scrollLeft === target.clientWidth) setIsInactive({ right: true });
+    else setIsInactive(null);
+  }
+
+
+  const left = () => slider.current.scrollLeft -= scrollLength;
+  const right = () => slider.current.scrollLeft += scrollLength;
+
   return (
     <StyledOfferSlider>
       {offerGroup.offers.map((offerElement, index) => (<Offer key={index} isGrouped={true} {...offerElement} />))}
