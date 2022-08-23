cfad437f90cefcb03d6c6f0452b95f63ade22c9d
Amit-Khobragade <amitkh1102@outlook.com>
2022-08-23 13:41:59 +0530

    Added secondary background color to searchBar

--- src/components/SearchBox/SearchBox.styles.js
+++ src/components/SearchBox/SearchBox.styles.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 const StyledInput = styled.input`
   background: url(assets/search.svg) no-repeat 5% center/1rem;
+  background-color: var(--secondary-clr);
   border: 1px solid;
   border-radius: 1rem;
   box-shadow: inset 0.2rem 0.2rem 0.25rem rgb(255 255 255 / 50%), inset -3px -3px 4px rgb(0 0 0 / 25%);
